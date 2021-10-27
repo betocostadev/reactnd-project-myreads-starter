e5ab5edcdf6abd30012ebc1aa78581bf4316fd8f
betocostadev <robertomarx@gmail.com>
2021-10-27 18:46:04 -0300

    added start files, router and divided some components

---
+++ src/pages/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchBar from '../components/SearchBar'
+
+const Search = () => {
+    return (
+        <div className="search-books">
+          <SearchBar />
+        </div>            
+    )
+}
+
+export default Search
